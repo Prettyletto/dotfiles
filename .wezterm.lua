ff68c940abf84744de38044b3cd6e2668253e9ee
Daniel Teixeira <danielltw2@gmail.com>
2025-04-23 14:45:47 -0300

    new dots

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,25 +6,27 @@ local config = wezterm.config_builder()
 
 --Fonts
 
-config.font = wezterm.font("ShureTechMono Nerd Font")
-config.font_size = 12
--- config.harfbuzz_features = {
--- 	"calt=1", -- Contextual Alternates
--- 	"clig=1", -- Contextual Ligatures
--- 	"liga=1", -- Standard Ligatures
--- }
+config.font = wezterm.font("ShureTechMonoNerdFontMono")
+config.font_size = 14
+config.harfbuzz_features = {
+	"calt=1", -- Contextual Alternates
+	"clig=1", -- Contextual Ligatures
+	"liga=1", -- Standard Ligatures
+}
 
 --Windows
-config.window_background_image = "/home/prettyletto/Documents/Themes/wallpaperflare.com_wallpaper.jpg"
+config.window_background_image = "/home/administrator/Downloads/terminal.jpg"
 config.window_background_image_hsb = {
-	brightness = 0.2,
+	brightness = 0.1,
+	saturation = 0.8,
 }
 config.enable_tab_bar = false
 config.window_decorations = "TITLE | RESIZE"
 config.window_background_opacity = 1
 
 -- Themes
-config.color_scheme = "Kanagawa Dragon (Gogh)"
+-- config.color_scheme = "Kanagawa Dragon (Gogh)"
+config.color_scheme = "Glacier"
 config.text_background_opacity = 1
 
 --Keybindings
